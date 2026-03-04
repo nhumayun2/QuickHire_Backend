68b7af2e9a3886d88348c33ee005a4a19b5de638
nhumayun2 <nhumayun291@gmail.com>
2026-03-04 20:41:30 +0600

    added routes and error handler to server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import mainRoutes from "./mainroute/index.js";
+import { errorHandler } from "./errors/errorHandler.js";
 
 dotenv.config();
 
@@ -9,6 +11,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", mainRoutes);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
